487febad0d0b94b495ef316991bc2e4b1732e0fe
Yumi1298 <nhb12980712@gmail.com>
2024-12-03 23:32:37 +0800

    環境

--- data/my_app_clients.sql
+++ data/my_app_clients.sql
@@ -18,8 +18,8 @@
 --
 -- Table structure for table `clients`
 --
+CREATE database `my_app`;
 
-DROP TABLE IF EXISTS `clients`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clients` (
